ad566bc6d335e5872a82c18293816776e2510c2f
Andy Delaney <andy.delaney@bjss.com>
2021-09-08 21:24:12 +0100

    Fixed spring boot versions

--- src/test/java/org/ag/springboot/services/ServiceAppTest.java
+++ src/test/java/org/ag/springboot/services/ServiceAppTest.java
@@ -1,23 +1,18 @@
 package org.ag.springboot.services;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class ServiceAppTest {
 
-//    @Autowired
-//    private ServiceApp serviceApp;
+class ServiceAppTest {
 
-//    @Test
-//    void request() {
-//
-//        assertEquals("Hello!! This is a string from " + serviceApp, serviceApp.request());
-//    }
+    private ServiceApp serviceApp;
+
+    @Test
+    void request() {
+
+        assertEquals("Hello!! This is a string from ???", new ServiceApp().request());
+    }
 }
